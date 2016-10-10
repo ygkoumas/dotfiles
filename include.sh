6b2a4bf9d5a3e8612a79a68bf36e846b36837042
Yannis Gkoumas <ygkoumas@gmail.com>
2016-10-11 00:37:15 +0100

    Add vimrc to the installer

--- include.sh
+++ include.sh
@@ -10,9 +10,12 @@ do
 	if [ -f $home_file ];
 	then
 		printf "\033[1;32m File $home_file: Append the following\n \033[0m"
-		# get the lines of the repo file that start with "####". Then modify and append them to the home file.
+		# get the lines of the repo file that start with '####' or '""""'. Then modify and append them to the home file.
 		# it appends only the location of the repo file into the home file on the right format.
 		append=$(grep "####" $file | sed -e s/"####"/""/g -e s#CURRENT_FILE_PATH#"$repo_file"#g )
+		append_quote=$(grep "\"\"\"\"" $file | sed -e s/"\"\"\"\""/""/g -e s#CURRENT_FILE_PATH#"$repo_file"#g )
+		append=$(printf "%s\n%s" "$append" "$append_quote")
+
 		echo "$append"
 		echo "" >> $home_file
 		echo "$append" >> $home_file
